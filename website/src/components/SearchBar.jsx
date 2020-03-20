8e6c2c70cf182dceb65fa6682d91f0d060c16b71
Jake Wheeler <jakewheeler17@gmail.com>
2020-03-19 22:10:10 -0400

    button has been added but could use some layout adjustments

--- website/src/components/SearchBar.jsx
+++ website/src/components/SearchBar.jsx
@@ -3,10 +3,12 @@ import { GameCard } from './GamesList';
 import { Input } from 'antd';
 import { Spin } from 'antd';
 import { List } from 'antd';
+import { Button } from 'antd';
 import axios from 'axios';
 import { getRequestConfig } from '../helpers/getRequestJwt';
 import { useFetchToken } from '../hooks/customHooks';
 import { useGameListState } from '../contexts/GamesListContext';
+import { useEffect } from 'react';
 
 const SearchBar = () => {
   const [isSearching, setIsSearching] = useState(false);
@@ -51,7 +53,47 @@ const SearchBar = () => {
   );
 };
 
+const GoToTop = ({ isVisible }) => {
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
+  console.log(isVisible);
+
+  return (
+    <>
+      {isVisible && (
+        <Button
+          style={{ zIndex: 1, position: 'fixed', right: 20, bottom: 20 }}
+          type='primary'
+          shape='circle'
+          size='large'
+          onClick={scrollToTop}
+        >
+          ^
+        </Button>
+      )}
+    </>
+  );
+};
+
 const SearchResults = ({ isSearching, searchResults }) => {
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [setIsScrolling]);
+
+  const handleScroll = () => {
+    if (window.pageYOffset !== 0) {
+      setIsScrolling(true);
+    } else {
+      setIsScrolling(false);
+    }
+  };
+
   if (isSearching && searchResults.length <= 0) {
     return (
       <div>
@@ -66,16 +108,23 @@ const SearchResults = ({ isSearching, searchResults }) => {
   if (!searchResults?.length || !isSearching) return <div></div>;
 
   return (
-    <List
-      className='search-results'
-      itemLayout='horizontal'
-      dataSource={searchResults}
-      renderItem={game => (
-        <List.Item key={game.id} className='search-item'>
-          <GameCard game={game} />
-        </List.Item>
-      )}
-    />
+    <div className='list-container' style={{ position: 'relative' }}>
+      <div className='scroll-icon'>
+        <GoToTop isVisible={isScrolling} />
+      </div>
+      <div className='list-parent'>
+        <List
+          className='search-results'
+          itemLayout='horizontal'
+          dataSource={searchResults}
+          renderItem={game => (
+            <List.Item key={game.id} className='search-item'>
+              <GameCard game={game} />
+            </List.Item>
+          )}
+        />
+      </div>
+    </div>
   );
 };
 
